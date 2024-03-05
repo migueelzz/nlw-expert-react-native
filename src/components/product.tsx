6f6b3b7a0361b30c393af5b2689acbc615d636a5
migueelzz <miguellemes005@gmail.com>
2024-03-05 20:37:15 -0300

    :rocket: nlw-expert: react native

---
+++ src/components/product.tsx
@@ -0,0 +1,39 @@
+import { Image, ImageProps, Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
+
+import { forwardRef } from "react"
+
+interface ProductDataProps {
+    title: string
+    description: string
+    thumbnail: ImageProps
+    quantity?: number
+}
+
+interface ProductProps extends TouchableOpacityProps {
+    data: ProductDataProps
+}
+
+export const Product = forwardRef<TouchableOpacity, ProductProps>(({ data, ...rest }, ref) => {
+    return (
+        <TouchableOpacity 
+        ref={ref}
+            className="w-full flex-row items-center pb-4"
+            {...rest}
+        >
+            <Image 
+                className="w-20 h-20 rounded-md"
+                source={data.thumbnail} 
+            />
+
+            <View className="flex-1 ml-3">
+                <View className="flex-row items-center">
+                    <Text className="text-slate-100 font-subtitle text-base flex-1">{data.title}</Text>
+
+                    {data.quantity && <Text className="text-slate-400 font-subtitle text-sm">x {data.quantity}</Text>}
+                </View>
+
+                <Text className="text-slate-400 text-xs leading-5 mt-0.5">{data.description}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+})
